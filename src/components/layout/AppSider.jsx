a0a89307d07211acca0246ae7e63253392c9fa26
Dmitry <dmitry27zh@gmail.com>
2024-03-04 18:12:58 +0500

    Add loader

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,4 +1,4 @@
-import { Layout, Card, Statistic, List, Typography } from 'antd'
+import { Layout, Card, Statistic, List, Typography, Spin } from 'antd'
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
 import { useEffect, useState } from 'react'
 import { fetchAssets, fetchCrypto } from '../../api'
@@ -32,40 +32,44 @@ export default function AppSider() {
     preload()
   }, [])
 
-  return (
-    <Layout.Sider width="25%" style={siderStyle}>
-      <Card style={{ marginBottom: '1rem' }}>
-        <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{ color: '#3f8600' }}
-          prefix={<ArrowUpOutlined />}
-          suffix="%"
-        />
-        <List
-          size="small"
-          header={<div>Header</div>}
-          footer={<div>Footer</div>}
-          bordered
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text> {item}
-            </List.Item>
-          )}
-        />
-      </Card>
-      <Card>
-        <Statistic
-          title="Idle"
-          value={9.3}
-          precision={2}
-          valueStyle={{ color: '#cf1322' }}
-          prefix={<ArrowDownOutlined />}
-          suffix="%"
-        />
-      </Card>
-    </Layout.Sider>
-  )
+  if (isLoading) {
+    return <Spin fullscreen />
+  } else {
+    return (
+      <Layout.Sider width="25%" style={siderStyle}>
+        <Card style={{ marginBottom: '1rem' }}>
+          <Statistic
+            title="Active"
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<ArrowUpOutlined />}
+            suffix="%"
+          />
+          <List
+            size="small"
+            header={<div>Header</div>}
+            footer={<div>Footer</div>}
+            bordered
+            dataSource={data}
+            renderItem={(item) => (
+              <List.Item>
+                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+              </List.Item>
+            )}
+          />
+        </Card>
+        <Card>
+          <Statistic
+            title="Idle"
+            value={9.3}
+            precision={2}
+            valueStyle={{ color: '#cf1322' }}
+            prefix={<ArrowDownOutlined />}
+            suffix="%"
+          />
+        </Card>
+      </Layout.Sider>
+    )
+  }
 }
