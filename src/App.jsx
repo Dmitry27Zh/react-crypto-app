93ae8a6ef06b9ab2908d15d87b3da909a72c241f
Dmitry <dmitry27zh@gmail.com>
2024-03-04 15:38:12 +0500

    Add Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import './App.css'
+import { Layout } from 'antd'
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 60,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+}
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 'calc(100vh - 60px)',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#001529',
+}
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+}
 
 function App() {
-  return <h1>Hi!</h1>
+  return (
+    <Layout>
+      <Layout.Header style={headerStyle}>Header</Layout.Header>
+      <Layout>
+        <Layout.Sider width="25%" style={siderStyle}>
+          Sider
+        </Layout.Sider>
+        <Layout.Content style={contentStyle}>Content</Layout.Content>
+      </Layout>
+    </Layout>
+  )
 }
 
 export default App
