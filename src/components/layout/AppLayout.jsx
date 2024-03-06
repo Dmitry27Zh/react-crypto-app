7be8c4e0fdea45f1d4dd5b57ca0f98a028f1ace4
Dmitry <dmitry27zh@gmail.com>
2024-03-06 15:47:49 +0500

    Extract AppLayout

---
+++ src/components/layout/AppLayout.jsx
@@ -0,0 +1,23 @@
+import { Layout, Spin } from 'antd'
+import AppHeader from './AppHeader'
+import AppSider from './AppSider'
+import AppContent from './AppContent'
+import { useContext } from 'react'
+import { CryptoContext } from '../../context/crypto-context'
+
+export default function AppLayout() {
+  const { isLoading } = useContext(CryptoContext)
+  if (isLoading) {
+    return <Spin fullscreen />
+  } else {
+    return (
+      <Layout>
+        <AppHeader />
+        <Layout>
+          <AppSider />
+          <AppContent />
+        </Layout>
+      </Layout>
+    )
+  }
+}
--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,4 +1,4 @@
-import { Layout, Card, Statistic, List, Typography, Spin, Tag } from 'antd'
+import { Layout, Card, Statistic, List, Typography, Tag } from 'antd'
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
 import { useContext } from 'react'
 import { capitalize } from '../../utils'
@@ -9,57 +9,51 @@ const siderStyle = {
 }
 
 export default function AppSider() {
-  const { isLoading, assets } = useContext(CryptoContext)
+  const { assets } = useContext(CryptoContext)
 
-  if (isLoading) {
-    return <Spin fullscreen />
-  } else {
-    return (
-      <Layout.Sider width="25%" style={siderStyle}>
-        {assets.map((asset) => (
-          <Card key={asset.id} style={{ marginBottom: '1rem' }}>
-            <Statistic
-              title={capitalize(asset.id)}
-              value={asset.totalAmount}
-              precision={2}
-              valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322' }}
-              prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
-              suffix="$"
-            />
-            <List
-              size="small"
-              bordered
-              dataSource={[
-                {
-                  title: 'Total Profit',
-                  value: asset.totalProfit,
-                  withTag: true,
-                },
-                {
-                  title: 'Asset Amount',
-                  value: asset.amount,
-                  isPlain: true,
-                },
-              ]}
-              renderItem={(item) => (
-                <List.Item>
-                  <span>{item.title}</span>
-                  <span style={{ display: 'flex', gap: '0.5rem' }}>
-                    {item.withTag && <Tag color={asset.grow ? 'green' : 'red'}>{asset.growPercent}%</Tag>}
-                    {item.isPlain ? (
-                      item.value
-                    ) : (
-                      <Typography.Text type={asset.grow ? 'success' : 'danger'}>
-                        {item.value.toFixed(2)}$
-                      </Typography.Text>
-                    )}
-                  </span>
-                </List.Item>
-              )}
-            />
-          </Card>
-        ))}
-      </Layout.Sider>
-    )
-  }
+  return (
+    <Layout.Sider width="25%" style={siderStyle}>
+      {assets.map((asset) => (
+        <Card key={asset.id} style={{ marginBottom: '1rem' }}>
+          <Statistic
+            title={capitalize(asset.id)}
+            value={asset.totalAmount}
+            precision={2}
+            valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322' }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix="$"
+          />
+          <List
+            size="small"
+            bordered
+            dataSource={[
+              {
+                title: 'Total Profit',
+                value: asset.totalProfit,
+                withTag: true,
+              },
+              {
+                title: 'Asset Amount',
+                value: asset.amount,
+                isPlain: true,
+              },
+            ]}
+            renderItem={(item) => (
+              <List.Item>
+                <span>{item.title}</span>
+                <span style={{ display: 'flex', gap: '0.5rem' }}>
+                  {item.withTag && <Tag color={asset.grow ? 'green' : 'red'}>{asset.growPercent}%</Tag>}
+                  {item.isPlain ? (
+                    item.value
+                  ) : (
+                    <Typography.Text type={asset.grow ? 'success' : 'danger'}>{item.value.toFixed(2)}$</Typography.Text>
+                  )}
+                </span>
+              </List.Item>
+            )}
+          />
+        </Card>
+      ))}
+    </Layout.Sider>
+  )
 }
