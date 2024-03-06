e48649cc72ad817635b6617e262e47fd886c4401
Dmitry <dmitry27zh@gmail.com>
2024-03-06 18:07:45 +0500

    Add AppHeader content

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,14 +1,53 @@
-import { Layout } from 'antd'
+import { Layout, Select, Space, Button } from 'antd'
+import { useCrypto } from '../../context/crypto-context'
+import { useEffect, useState } from 'react'
 
 const headerStyle = {
   textAlign: 'center',
-  color: '#fff',
+  width: '100%',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
+  padding: '1rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 }
 
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>
+  const [select, setSelect] = useState(false)
+  const { crypto } = useCrypto()
+  useEffect(() => {
+    const keydownHandler = (event) => {
+      if (event.key === '/') {
+        setSelect((prevState) => !prevState)
+      }
+    }
+    document.addEventListener('keydown', keydownHandler)
+
+    return () => {
+      document.removeEventListener('keydown', keydownHandler)
+    }
+  }, [])
+  function handleSelect() {}
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{ width: 250 }}
+        open={select}
+        onSelect={handleSelect}
+        value="press / to open"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img style={{ width: 20 }} src={option.data.icon} alt={option.data.label} /> {option.data.label}
+          </Space>
+        )}
+      />
+      <Button type="primary">Add asset</Button>
+    </Layout.Header>
+  )
 }
