0abf56ea0832ea2eeb830682ab67547fd974be2f
Dmitry <dmitry27zh@gmail.com>
2024-03-10 11:09:38 +0500

    Add PortfolioChart

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,5 +1,7 @@
 import { Layout, Typography } from 'antd'
 import { useCrypto } from '../../context/crypto-context'
+import PortfolioChart from '../PortfolioChart'
+import AssetsTable from '../AssetsTable'
 
 const contentStyle = {
   textAlign: 'center',
@@ -23,6 +25,8 @@ export default function AppContent() {
       <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
         Portfolio: {sum}$
       </Typography.Title>
+      <PortfolioChart />
+      <AssetsTable />
     </Layout.Content>
   )
 }
